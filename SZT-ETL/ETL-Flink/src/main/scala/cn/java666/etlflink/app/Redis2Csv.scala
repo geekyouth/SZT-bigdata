01b13384334051d3dc74055a99b4ce68bf1eb737
Geek <forsupergeeker@gmail.com>
2020-04-14 05:24:53 +0800

    redis szt:pageJson 抽取元数据到 csv

---
+++ SZT-ETL/ETL-Flink/src/main/scala/cn/java666/etlflink/app/Redis2Csv.scala
@@ -0,0 +1,30 @@
+package cn.java666.etlflink.app
+
+import java.util.Properties
+
+import cn.java666.etlflink.sink.MyCsvSinkFun
+import cn.java666.etlflink.source.MyRedisSourceFun
+import org.apache.flink.streaming.api.scala._
+
+/**
+ * @author Geek
+ * @date 2020-04-14 04:35:36
+ *
+ * redis szt:pageJson 抽取元数据到 csv 
+ * 
+ */
+object Redis2Csv {
+	def main(args: Array[String]): Unit = {
+		val env = StreamExecutionEnvironment.getExecutionEnvironment
+		env.setParallelism(1)
+		
+		val prop = new Properties
+		prop.load(ClassLoader.getSystemResourceAsStream("kafka.properties"))
+		
+		val s = env.addSource(new MyRedisSourceFun)
+		
+		s.addSink(new MyCsvSinkFun)
+		
+		env.execute("Redis2Csv")
+	}
+}
