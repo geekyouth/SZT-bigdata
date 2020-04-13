19288a02d758d411a534f60ff8d047f1379af9ad
Geek <forsupergeeker@gmail.com>
2020-04-13 18:47:01 +0800

    Flink sink to Redis 💦🆗🆗🆗

---
+++ SZT-ETL/ETL-Flink/src/main/scala/cn/java666/etlflink/sink/MyRedisSink.scala
@@ -0,0 +1,45 @@
+package cn.java666.etlflink.sink
+
+import com.alibaba.fastjson.{JSON, JSONObject}
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.redis.RedisSink
+import org.apache.flink.streaming.connectors.redis.common.config.FlinkJedisPoolConfig
+import org.apache.flink.streaming.connectors.redis.common.mapper.{RedisCommand, RedisCommandDescription, RedisMapper}
+
+/**
+ * @author Geek
+ * @date 2020-04-13 18:16:27
+ *
+ * redis 排序去重 ok
+ *
+ */
+object MyRedisSink {
+	def main(args: Array[String]): Unit = {
+		val env = StreamExecutionEnvironment.getExecutionEnvironment
+		env.setParallelism(1)
+		
+		val s = env.readTextFile("""D:\0-data\opendata.sz.gov.cn\SZT2018Record\2018record3.jsons""")
+			.filter(_.nonEmpty)
+			.map(x => {
+				JSON.parseObject(x)
+			})
+		
+		//定义 redis 参数
+		val jedis = new FlinkJedisPoolConfig.Builder().setHost("localhost").build()
+		
+		//存到 redis sink
+		s.addSink(new RedisSink(jedis, new MyRedisSinkFun))
+		
+		env.execute("MyRedisSink")
+	}
+}
+
+case class MyRedisSinkFun() extends RedisMapper[JSONObject] {
+	override def getCommandDescription: RedisCommandDescription = {
+		new RedisCommandDescription(RedisCommand.HSET, "szt:pageJson")
+	}
+	
+	override def getKeyFromData(data: JSONObject): String = data.getIntValue("page").toString
+	
+	override def getValueFromData(data: JSONObject): String = data.toString
+}
