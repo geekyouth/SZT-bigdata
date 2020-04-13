3904cfc8a6b56ea970a7d4ea515e38f3407748bf
Geek <forsupergeeker@gmail.com>
2020-04-13 23:09:16 +0800

    核心依赖🎈🎈🎈

---
+++ SZT-ETL/ETL-SpringBoot/src/test/java/cn/java666/etlspringboot/EtlSAppTests.java
@@ -0,0 +1,15 @@
+package cn.java666.etlspringboot;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class EtlSAppTests {
+	
+	@Test
+	public void contextLoads() {
+	}
+}
