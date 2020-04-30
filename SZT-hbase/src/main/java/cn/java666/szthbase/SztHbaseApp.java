c5a25f905026578c6fc9076700cc6815e8b00b97
Geek <forsupergeeker@gmail.com>
2020-04-30 13:55:55 +0800

    新增模块：SZT-hbase 集成 springboot🎁🎁🎁

---
+++ SZT-hbase/src/main/java/cn/java666/szthbase/SztHbaseApp.java
@@ -0,0 +1,12 @@
+package cn.java666.szthbase;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SztHbaseApp {
+	
+	public static void main(String[] args) {
+		SpringApplication.run(SztHbaseApp.class, args);
+	}
+}
