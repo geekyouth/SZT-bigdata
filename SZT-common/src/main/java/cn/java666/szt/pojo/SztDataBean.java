2c6ef1272786272312c2218826439d17f292c264
Geek <forsupergeeker@gmail.com>
2020-04-30 14:05:02 +0800

    新增模块：SZT-hbase 集成 springboot🎁🎁🎁 pojo

---
+++ SZT-common/src/main/java/cn/java666/szt/pojo/SztDataBean.java
@@ -0,0 +1,25 @@
+package cn.java666.szt.pojo;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+/**
+ * @author Geek
+ * @date 2020-04-29 23:15:45
+ */
+// @Builder
+@AllArgsConstructor
+@Data
+public class SztDataBean {
+	private String deal_date;
+	private String close_date;
+	private String card_no;
+	private String deal_value;
+	private String deal_type;
+	private String company_name;
+	private String car_no;
+	private String station;
+	private String conn_mark;
+	private String deal_money;
+	private String equ_no;
+}
