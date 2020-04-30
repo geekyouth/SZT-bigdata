6c2454cef8cb3022e2673e30afa3296d34c946cf
Geek <forsupergeeker@gmail.com>
2020-05-01 01:35:51 +0800

    TODO 假装休息一会，如果客户觉得速度太慢，可以加钱优化！！！但是这里我们真的需要休息，因为速度太快机器会卡住🙄🙄🙄甚至崩溃

--- SZT-ETL/ETL-Flink/src/main/scala/cn/java666/etlflink/app/Redis2Kafka.scala
+++ SZT-ETL/ETL-Flink/src/main/scala/cn/java666/etlflink/app/Redis2Kafka.scala
@@ -7,12 +7,14 @@ import org.apache.flink.api.common.serialization.SimpleStringSchema
 import org.apache.flink.streaming.api.scala._
 import org.apache.flink.streaming.connectors.kafka.FlinkKafkaProducer011
 
+import scala.util.Random
+
 /**
  * @author Geek
  * @date 2020-04-14 04:35:36
  *
  * redis szt:pageJson 抽取源数据到 kafka
- * 
+ *
  */
 object Redis2Kafka {
 	def main(args: Array[String]): Unit = {
@@ -23,6 +25,11 @@ object Redis2Kafka {
 		prop.load(ClassLoader.getSystemResourceAsStream("kafka.properties"))
 		
 		val s = env.addSource[String](new MyRedisSourceFun)
+			.map(x => {
+				// TODO 假装休息一会，如果客户觉得速度太慢，可以加钱优化！！！但是这里我们真的需要休息，因为速度太快机器会卡住🙄🙄🙄甚至崩溃
+				//Thread.sleep(Random.nextInt(10)) 
+				x
+			})
 		
 		s.addSink(
 			new FlinkKafkaProducer011(
