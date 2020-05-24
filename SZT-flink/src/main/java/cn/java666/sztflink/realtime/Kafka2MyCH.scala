39844e71cbaf60d2f8649c8833894f8255360ea7
Geek <forsupergeeker@gmail.com>
2020-05-24 23:35:47 +0800

    flink 读取 kafka 存到 clickhouse【自定义】

---
+++ SZT-flink/src/main/java/cn/java666/sztflink/realtime/Kafka2MyCH.scala
@@ -0,0 +1,39 @@
+package cn.java666.sztflink.realtime
+
+import java.util.Properties
+
+import cn.java666.sztflink.realtime.sink.MyClickhouseSinkFun
+import org.apache.flink.api.common.serialization.SimpleStringSchema
+import org.apache.flink.streaming.api.scala._
+import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer011
+import org.junit.Test
+
+/**
+ * @author Geek
+ * @date 2020-05-24 18:14:27
+ *
+ * flink 读取 kafka 存到 ch【自定义】
+ */
+case class Kafka2MyCH() {
+	
+	@Test
+	def test1() {
+		val env = StreamExecutionEnvironment.getExecutionEnvironment
+		env.setParallelism(1)
+		
+		val kafka_prop = new Properties
+		kafka_prop.setProperty("bootstrap.servers", "cdh231:9092")
+		kafka_prop.setProperty("group.id", "consumer-group-flink")
+		
+		env.addSource[String](new FlinkKafkaConsumer011("topic-flink-szt", new SimpleStringSchema, kafka_prop))
+			.name("kafka-source")
+			.map(x => {
+				//Thread.sleep(1000)
+				x
+			})
+			.addSink(new MyClickhouseSinkFun("cdh231"))
+			.name("ch-sink")
+		
+		env.execute("Kafka2CH")
+	}
+}
